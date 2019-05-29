920e9a561f4618b1f46c1366250907924a72cdf9
Matagh <mathis.agb@gmail.com>
2019-05-29 18:23:52 +0200

    Update CodersStrikeBack.cpp
    Wood League Division 1

--- CodersStrikeBack.cpp
+++ CodersStrikeBack.cpp
@@ -2,43 +2,100 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <cmath>
 
 using namespace std;
-
+#define PI 3.14159265
 /**
- * This code automatically collects game data in an infinite loop.
- * It uses the standard input to place data into the game variables such as x and y.
- * YOU DO NOT NEED TO MODIFY THE INITIALIZATION OF THE GAME VARIABLES.
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
  **/
+struct Vector2
+{
+	float x;
+	float y;
 
+	Vector2(float x, float y) : 
+		x(x), y(y) {}
+
+	// Arithmetic operators
+	Vector2 operator + (const Vector2& v) const { return Vector2(x + v.x, y + v.y); }
+	Vector2 operator - (const Vector2& v) const	{ return Vector2(x - v.x, y - v.y); }
+	Vector2 operator * (const float s) const { return Vector2(s*x, s*y); }
+	Vector2 operator * (const Vector2& v) const	{ return Vector2(x*v.x, y*v.y); }
+	Vector2 operator / (const float s) const {
+		float r = 1 / s;
+		return *this * r;
+	}
+
+	bool operator == (const Vector2& v) { return (x == v.x) && (y == v.y); }
+	bool operator != (const Vector2& v) { return (x != v.x) || (y != v.y); }
+};
+
+/* Arithmetic methods*/
+float Dot(const Vector2& u, const Vector2& v) {
+	return u.x * v.x + u.y * v.y;
+}
+
+float Length(const Vector2& v) {
+	return sqrt(Dot(v, v));
+}
+
+Vector2 Normalize(const Vector2& v) {
+	return v / Length(v);
+}
+
+Vector2 Rotate(const Vector2& v, float angle) {
+	float rad = angle * PI / 180;
+	double cosAngle = cos(rad);
+	double sinAngle = sin(rad);
+
+	return Vector2(v.x * cosAngle - v.y * sinAngle, v.y * cosAngle + v.x * sinAngle);
+}
 int main()
 {
+	const int LimitAngle = 90;
+	const int MinSteerAngle = 1;
 
     // game loop
     while (1) {
-        int x; // x position of your pod
-        int y; // y position of your pod
+        int x;
+        int y;
         int nextCheckpointX; // x position of the next check point
         int nextCheckpointY; // y position of the next check point
-        cin >> x >> y >> nextCheckpointX >> nextCheckpointY; cin.ignore();
- 
-        // Write an action using cout. DON'T FORGET THE "<< endl"
-        // To debug: cerr << "Debug messages..." << endl;
+        int nextCheckpointDist; // distance to the next checkpoint
+        int nextCheckpointAngle; // angle between your pod orientation and the direction of the next checkpoint
+        cin >> x >> y >> nextCheckpointX >> nextCheckpointY >> nextCheckpointDist >> nextCheckpointAngle; cin.ignore();
+        int opponentX;
+        int opponentY;
+        cin >> opponentX >> opponentY; cin.ignore();
 
-        if(x <= nextCheckpointX - 600)
-	        x++;
-	    else if (x > nextCheckpointX + 600)
-			x--;
-			
-        if(y <= nextCheckpointY - 600)
-		    y++;
-		else if (y > nextCheckpointY + 600)
-		    y--;
-        // Edit this line to output the target position
-        // and thrust (0 <= thrust <= 100)
+		int thrust = 100;
+
+		if (nextCheckpointAngle <= -MinSteerAngle || nextCheckpointAngle >= MinSteerAngle)
+		{
+			// Seek correct target
+			Vector2 wantedDirection(nextCheckpointX - x, nextCheckpointY - y);
+			wantedDirection = Normalize(wantedDirection);
+
+			Vector2 currentDirection = Rotate(wantedDirection, -nextCheckpointAngle);
+			currentDirection = Normalize(currentDirection);
+
+			Vector2 steeringDirection = wantedDirection - currentDirection;
+			steeringDirection = Normalize(steeringDirection) * 100;
+
+			nextCheckpointX += steeringDirection.x;
+			nextCheckpointY += steeringDirection.y;
+
+			// Thrusting down
+			if (nextCheckpointAngle <= -LimitAngle || nextCheckpointAngle >= LimitAngle)
+			{
+				thrust = 0;
+			}
+		}
+        // You have to output the target position
+        // followed by the power (0 <= thrust <= 100)
         // i.e.: "x y thrust"
-        cout << nextCheckpointX << " " << nextCheckpointY << " 65" << endl;
-
-
+        cout << nextCheckpointX << " " << nextCheckpointY << " " << thrust << endl;
     }
 }
